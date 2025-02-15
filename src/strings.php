5ed6da14167556413bd4c7525b719f346e1e95c0
Simeon Franklin <simeonf@gmail.com>
2025-02-15 10:14:27 -0800

    add a few more string tests

--- src/strings.php
+++ src/strings.php
@@ -1,8 +1,35 @@
 <?php
+/* Read
+   - https://www.php.net/manual/en/language.types.string.php
+   - https://www.php.net/manual/en/language.operators.string.php
+   - https://www.php.net/manual/en/ref.strings.php
+   - https://flatcoding.com/tutorials/php-programming/php-string/
+*/
 
+// Add two strings that are passed in
 function add2strings($a, $b){
-  return $a + $b;
+    return $a + $b;
 }
 
+// Put a space between first name and last name
+// Make sure first and last are both capitalized
+function name($first, $last){
+    return "";
+}
 
+// make an rgb string out of variables "rgb(200, 100, 5)"
+// don't allow an illegal value rgb(256, 300, 400)
+function rgb($r, $g, $b){
+    return "rgb(,,)";
+}
+
+// If long text is longer than length, insert line breaks "\n"
+function wrap($long_text, $length){
+    return $long_text;
+}
+
+//Only print the string if there are more "e"s than "a"s.
+function more_e($text){
+    return text;
+}
 ?>
--- tests/StringTest.php
+++ tests/StringTest.php
@@ -8,9 +8,39 @@ use PHPUnit\Framework\TestCase;
 final class StringTest extends TestCase
 {
     public function test_add2strings(){
+
         $result = add2strings("Simeon", "Franklin");
         $this->assertSame($result, "SimeonFranklin");
         $result = add2strings("Andrew", "Franklin");
         $this->assertSame($result, "AndrewFranklin");
+    }
+
+    public function test_name(){
+
+        $result = name("simeon", "franklin");
+        $this->assertSame($result, "Simeon Franklin");
+    }
+
+    public function test_rgb(){
+        $result = rgb(200, 100, 0);
+        $this->assertSame($result, "rgb(200, 100, 0)");
+        $result = rgb(200, 400, 0);
+        $this->assertSame($result, "rgb(200, 255, 0)");
+    }
+
+    public function test_wrap(){
+        $text = "This is a long piece of text";
+        $result = wrap($text, 15);
+        $this->assertSame($result, "This is a long piece \nof text");
+        $text = "This is a an even longer piece of text which might repeat.";
+        $result = wrap($text, 15);
+        $this->assertSame($result, "This is a an ev\nen longer piece\n of text which\nmight repeat.");
+    }
+
+   public function test_more_e(){
+        $result = more_e("Old McDonald had a Farm");
+        $this->assertSame($result, "");
+        $result = more_e("EIEIO!");
+        $this->assertSame($result, "EIEIO!");
     }
 }
